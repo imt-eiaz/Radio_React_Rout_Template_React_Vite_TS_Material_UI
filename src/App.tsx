38a3c19f9e534c27a4d52e7dde3f7dc90a69ca2e
Imtiaz Khan <imteiaz@gmail.com>
2024-12-14 17:47:49 +0000

    C3

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,39 @@ import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
 import { Grid2 } from "@mui/material";
 
+import { BrowserRouter as Router, Route, Link } from "react-router";
+
 export default function App() {
   return (
     <>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Route path="/about">
+          <App />
+        </Route>
+        {/* <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route> */}
+      </div>
+
       <Grid2>
         <FormControl>
           <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
